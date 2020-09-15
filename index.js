9e7205194ff2a7dd661c2258512cae5cc342ceb3
SonMaxime <47661880+SonMaxime@users.noreply.github.com>
2020-09-15 19:03:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ const config = require("./config.js");
 
 let page = 0;
 client.page = page
+
+fs.readdir("./events/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    const event = require(`./events/{file}`);
+    let eventName = file.split(".")[0];
+  });
+});
+
+client.commands = new Enmap();
