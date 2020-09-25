afd7724e5fcf836a19d473f86a326f0424eab05c
SonMaxime <47661880+SonMaxime@users.noreply.github.com>
2020-09-25 23:22:57 +0200

    Update now.js

--- commands/now.js
+++ commands/now.js
@@ -1,13 +1,13 @@
+const MessageEmbed = require('discord.js');
+
 module.exports.run = (client, message) => {
     const queue = require('../liste.json');
-    
-    const Discord = require('discord.js');
 
     let song = queue.liste
     
     let musicfs = song[client.page];
 
-        const embed = new Discord.MessageEmbed()
+        const embed = new MessageEmbed()
         .addField('Joué actuellement :', `${musicfs.titre}`, false)
         .setTimestamp()
         message.channel.send(embed)
